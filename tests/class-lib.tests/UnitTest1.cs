3ac617bbc3f49240d2c3556244846d0fb4378e2f
Bernhard Richter <bernhard.richter@gmail.com>
2019-08-08 22:54:09 +0200

    Added ContainerFixture

--- tests/class-lib.tests/UnitTest1.cs
+++ tests/class-lib.tests/UnitTest1.cs
@@ -1,26 +1,18 @@
-using System;
 using class_lib.interfaces;
-using class_lib.services;
-using LightInject;
-using LightInject.xUnit2;
 using Xunit;
 
 namespace class_lib.tests
 {
 
-    public class UnitTest1
+    public class UnitTest1 : ContainerFixture
     {
+        public IService01 svc01;
 
-        public static void Configure(IServiceContainer container)
-        {
-            container.ScopeManagerProvider = new PerLogicalCallContextScopeManagerProvider();
+        public IService02 svc02;
 
-            container.Register<IService01, Service01>(new PerScopeLifetime());
-            container.Register<IService02, Service02>(new PerScopeLifetime());
-        }
 
-        [Theory, Scoped, InjectData]
-        public void Test1(IService01 svc01, IService02 svc02)
+        [Fact]
+        public void Test1()
         {
             var value = svc01.GenerateValue();
             var echo = svc02.Echo02(value);
